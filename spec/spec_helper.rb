1c97e40e3d66a67afee35d161bf20f5c282d1d3c
Robin Bowes <robin.bowes@yo61.com>
2015-07-17 00:17:15 +0100

    Add function to capture stdout for testing - not sure this is the best approach

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,14 @@ $: << File.join(File.dirname(__FILE__), '..', 'lib')
  
 require 'pry'
 require 'foo2bar'
+
+def capture_stdout(&block)
+  original_stdout = $stdout
+  $stdout = fake = StringIO.new
+  begin
+    yield
+  ensure
+    $stdout = original_stdout
+  end
+  fake.string
+end
