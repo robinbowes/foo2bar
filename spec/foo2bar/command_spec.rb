359818be4bed5fa3ac2639075444d489cfb88046
Robin Bowes <robin.bowes@yo61.com>
2015-07-17 00:17:56 +0100

    Attempt to construct a test to ensure the CLI app behaves correctly

--- spec/foo2bar/command_spec.rb
+++ spec/foo2bar/command_spec.rb
@@ -1,11 +1,18 @@
 require "spec_helper"
 require "foo2bar/command"
 
+
 describe "Foo2bar::Command" do
+
+  let(:cmd) { Foo2bar::Command.new("foo2bar") }
+
+  subject { cmd }
+
   context "when run without arguments" do
+    let(:expected_output) { 'Summary' }
+    let(:output) { capture_stdout { cmd.run([]) } }
     it "should display a usage summary" do
-      cmd = Foo2bar::Command.new("foo2bar")
-      cmd.run([])
+      output.should include(expected_output)
     end
   end
 end
